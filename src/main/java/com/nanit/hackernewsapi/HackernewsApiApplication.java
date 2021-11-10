6f90e4c0d720ec8b826bbcd600174102ca71d73c
rotemwald <waldr@post.bgu.ac.il>
2021-11-10 19:30:00 +0200

    first commit

---
+++ src/main/java/com/nanit/hackernewsapi/HackernewsApiApplication.java
@@ -0,0 +1,13 @@
+package com.nanit.hackernewsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackernewsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HackernewsApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nanit/hackernewsapi/HackernewsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nanit.hackernewsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackernewsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
