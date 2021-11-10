a9313d1719be4712792e723dd4d8fcb1eaa0a977
rotemwald <waldr@post.bgu.ac.il>
2021-11-10 20:08:51 +0200

    removed unnecessary new lines

--- src/main/java/com/nanit/hackernewsapi/HackernewsApiApplication.java
+++ src/main/java/com/nanit/hackernewsapi/HackernewsApiApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HackernewsApiApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(HackernewsApiApplication.class, args);
     }
-
 }
