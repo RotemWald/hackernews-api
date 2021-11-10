51ec37aae785251f3d3cb2aa19951f824bf223b4
rotemwald <waldr@post.bgu.ac.il>
2021-11-10 20:07:21 +0200

    added ResponseEntity to the PostController class

--- src/main/java/com/nanit/hackernewsapi/controllers/PostController.java
+++ src/main/java/com/nanit/hackernewsapi/controllers/PostController.java
@@ -3,6 +3,8 @@ package com.nanit.hackernewsapi.controllers;
 import com.nanit.hackernewsapi.entities.Post;
 import com.nanit.hackernewsapi.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +16,8 @@ public class PostController {
     private PostService service;
 
     @GetMapping
-    public Iterable<Post> getAllPosts() {
-        return service.getAllPosts();
+    public ResponseEntity<Iterable<Post>> getAllPosts() {
+        Iterable<Post> posts = service.getAllPosts();
+        return new ResponseEntity<>(posts, HttpStatus.OK);
     }
 }
