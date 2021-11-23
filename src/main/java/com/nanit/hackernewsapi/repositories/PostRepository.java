88d2fac4e8cba9d15f4f1e371c24dd25f9fb7915
rotemwald <waldr@post.bgu.ac.il>
2021-11-23 11:02:03 +0200

    changed score calculation formula

--- src/main/java/com/nanit/hackernewsapi/repositories/PostRepository.java
+++ src/main/java/com/nanit/hackernewsapi/repositories/PostRepository.java
@@ -9,7 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 public interface PostRepository extends CrudRepository<Post, Integer> {
     @Transactional
-    @Query(value = "UPDATE `post` SET `votes` = `votes` + 1, `score` = `votes` / POW(((UNIX_TIMESTAMP(NOW()) - UNIX_TIMESTAMP(`created_at`)) / 3600) + 2, 1.8) WHERE id = :id", nativeQuery = true)
+    @Query(value = "UPDATE `post` SET `votes` = `votes` + 1, `score` = (`votes` + 1) * (UNIX_TIMESTAMP(`created_at`) * 0.0000000001) WHERE id = :id", nativeQuery = true)
     @Modifying
     void vote(@Param(value = "id") Integer id);
 
